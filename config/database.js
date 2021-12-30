cf7612f7c0c62509a387f038721e0087307dd594
Bentagai <bentagai@protonmail.com>
2021-12-30 12:16:52 +0000

    test 4

--- config/database.js
+++ config/database.js
@@ -19,11 +19,9 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'bipolab'),
-      url: env('DATABASE_URL'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '1234'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
-/* DATABASE_URL=postgres://strapi_mz5d_user:wNuXC8mjGv7jkgWBSGTTjq9Qlo0xXtS1@dpg-c76nm79g7hp8rc6mqukg/strapi_mz5d */
