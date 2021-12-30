222dd6cc2e295ffe65cc79e25ae3bd50df86d241
Bentagai <bentagai@protonmail.com>
2021-12-30 09:12:43 +0000

    test

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'bipolab'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '1234'),
-      ssl: env.bool('DATABASE_SSL', false),
+      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });
