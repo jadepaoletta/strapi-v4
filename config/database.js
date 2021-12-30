d08b75ca035c45e6fd259eb144a46b352c709183
Bentagai <bentagai@protonmail.com>
2021-12-30 10:15:54 +0000

    produccion

--- config/database.js
+++ config/database.js
@@ -25,28 +25,3 @@ module.exports = ({ env }) => ({
   },
 });
 */
-
-/* Render Deployment
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-        },
-      },
-    },
-  };
-}; 
-*/
