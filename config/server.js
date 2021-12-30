0b5a27a7ac559e3392b86cc1b658957e98f6012b
Bentagai <bentagai@protonmail.com>
2021-12-30 11:28:06 +0000

    test 2

--- config/server.js
+++ config/server.js
@@ -1,6 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('DATABASE_URL')
 });
-/* DATABASE_URL=postgres://strapi_mz5d_user:wNuXC8mjGv7jkgWBSGTTjq9Qlo0xXtS1@dpg-c76nm79g7hp8rc6mqukg/strapi_mz5d */
